08c758ba1e1f434d585018d87036e7bdf0a4c020
Alex Avila <84880348+Alexisrodri@users.noreply.github.com>
2023-11-06 14:32:02 -0500

    impl isar como base de datos local

--- cinemapedia/lib/presentation/movies/movie_screen.dart
+++ cinemapedia/lib/presentation/movies/movie_screen.dart
@@ -1,6 +1,5 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
 import 'package:cinemapedia/presentation/providers/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -195,13 +194,13 @@ class _ActorsByMovie extends ConsumerWidget {
   }
 }
 
-class _CustomSliverAppBar extends StatelessWidget {
+class _CustomSliverAppBar extends ConsumerWidget {
   final Movie movie;
 
   const _CustomSliverAppBar({required this.movie});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final size = MediaQuery.of(context).size;
     return SliverAppBar(
       backgroundColor: Colors.black,
@@ -209,7 +208,9 @@ class _CustomSliverAppBar extends StatelessWidget {
       foregroundColor: Colors.white,
       actions: [
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            ref.watch(localStorageRepositoryProvider).toogleFavorite(movie);
+          },
           icon: const Icon(Icons.favorite_border),
           // icon: const Icon(
           //   Icons.favorite_outlined,
