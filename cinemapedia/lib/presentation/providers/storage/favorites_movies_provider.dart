a11cb4f9e09a50d86eae219ba1c2321cb6e1b41c
Alex Avila <84880348+Alexisrodri@users.noreply.github.com>
2023-11-13 17:03:07 -0500

    plugin generados por isar

--- cinemapedia/lib/presentation/providers/storage/favorites_movies_provider.dart
+++ cinemapedia/lib/presentation/providers/storage/favorites_movies_provider.dart
@@ -1,6 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/domain/repositories/local_storage_repository.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // final favoriteMoviesProvider = StateNotifierProvider((ref) {
@@ -12,4 +11,19 @@ class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
   final LocalStorageRepository localStorageRepository;
 
   StorageMoviesNotifier({required this.localStorageRepository}) : super({});
+
+  Future<void> loadNextPage() async {
+    final movies =
+        await localStorageRepository.loadMovies(offset: page * 10, limit: 20);
+    page++;
+
+    final tempMoviesMap = <int, Movie>{};
+    for (final movie in movies) {
+      tempMoviesMap[movie.id] = movie;
+    }
+
+    state = {...state, ...tempMoviesMap};
+
+    // return movies;
+  }
 }
